f7ff2b8a6f7436b08d02baf6c91728e2045bbaf7
OG <olgageletina@gmail.com>
2018-11-23 20:16:46 -0500

    added a 404

--- controllers/404.js
+++ controllers/404.js
@@ -1,8 +1,18 @@
-var template = require('../views/template-main');  
-exports.get = function(request, response) {  
-  response.writeHead(404, {
-    'Content-Type': 'text/html'
-  });
-  response.write(template.build("404 - Page not found", "Oh noes, it's a 404", "<p>This isn't the page you're looking for...</p>"));
-  response.end();
+var template = require('../views/template-404');
+
+exports.get = function(request, response) {
+    //second promise to generate the template
+    template.build()
+    .then((res) => {
+        response.writeHead(200, {
+        'Content-Type': 'text/html'});
+        response.write(res);
+        response.end();
+    }).catch((err) => {
+        response.writeHead(404, {
+            'Content-Type': 'text/html'
+        });
+        response.write(err);
+        response.end();
+    });
 };
---
+++ views/template-404.js
@@ -0,0 +1,39 @@
+var _ = require('underscore');
+var fs = require('fs');
+
+const generateData = function() {
+    var errorHTML = {};
+    return renderTemplate("headHTML", { path: "" })
+        .then(result => {
+            errorHTML.headerHTML = result;
+            return errorHTML;
+        })
+        .then(errorHTML => {
+            return renderTemplate("404", errorHTML)
+                .then(finalHTML => {
+                    return finalHTML.toString();
+                })
+        })
+    .catch(err => {
+        console.log('ERRRRRRRR', err);
+        throw err;
+    })
+};
+
+
+exports.build = function() {
+    return generateData();
+};
+
+const renderTemplate = function(_templateName, _data) {
+    return new Promise((resolve, reject) => {
+        return fs.readFile("./templates/" + _templateName + ".html", (err, _templateString) => {
+            if (err) reject(err);
+            var _template = _.template(_templateString.toString());
+            resolve(_template(_data));
+        })
+    })
+};
+
+
+// generateData().then((success) => console.log(success)).catch((error) => console.log(error));
